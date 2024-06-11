1aa91c8783bd2aae74fc2dc28352ecd6b5eaca1f
Max <7enidomaks@gmail.com>
2024-06-11 18:23:16 +0300

    Добавление новой функции; Присоединение к интерфейсу; Добавление комментариев ver=0.1

--- analyz.py
+++ analyz.py
@@ -1,39 +1,44 @@
 import json
 import pandas
 import pandas as pd
-import altair as alt
-import altair_viewer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import lyricsgenius
-import os.path
 import pymorphy3
 import stopwords
-
-import matplotlib as plt
-#from wordcloud import WordCloud
+from wordcloud import WordCloud
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from googletrans import Translator
+import matplotlib.pyplot as plt
+from fuzzywuzzy import process, fuzz
 
-def translate_text(text):
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+morph = pymorphy3.MorphAnalyzer()
+stop = stopwords.get_stopwords('ru')
+
+
+def translate_text(text):  # Функция, необходимая для перевода слов для оценки эмоциональной окраски
     translator = Translator()
     translation = translator.translate(text, dest='en')
     return translation.text
 
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-morph=pymorphy3.MorphAnalyzer()
-stop=stopwords.get_stopwords('ru')
-stopen=stopwords.get_stopwords('en')
-
-
-def down(author):
-    genius = lyricsgenius.Genius("Yj6AKvZjM2BDv-OMemcZ_cSCz2fubrAAB3PPV743bUpo1nzfAknSO0lKj-P_Jmrt", skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"])
+def down(author):  # Скачивание автора, которого нет в базе
+    genius = lyricsgenius.Genius("Yj6AKvZjM2BDv-OMemcZ_cSCz2fubrAAB3PPV743bUpo1nzfAknSO0lKj-P_Jmrt",
+                                 skip_non_songs=True,
+                                 excluded_terms=["(Remix)", "(Live)"])
     artist = genius.search_artist(author)
     artist.save_lyrics()
 
+# Проверка на совпадение автора, что позволяет уменьшить количество ошибок при вводе
 
-def inf(autohr): #Слздаём DataFrame файл, который содежрит информацию о песнях.
+
+def get_author(author, list_of_authors):
+    author_cool, score = process.extractOne(author, list_of_authors, scorer=fuzz.token_sort_ratio)
+    return author_cool if score > 30 else None
+
+
+def inf(autohr):  # Создаём DataFrame файл, который содежрит информацию о песнях.
     with open(f"Lyrics_{autohr}.json") as f:
         datao = json.load(f)
         listi = []
@@ -63,7 +68,7 @@ def inf(autohr): #Слздаём DataFrame файл, который содежр
     df = pandas.DataFrame(listi)
     df.dropna(inplace=True)
     df.head()
-    songs_b = df.groupby(df.album).size().reset_index(name='counts') #Сортируем песни по албомам исполнителя
+    songs_b = df.groupby(df.album).size().reset_index(name='counts')  # Сортируем песни по албомам исполнителя
     songs_b.sort_values(by="counts", ascending=False)
     songs_b.drop(songs_b[songs_b.counts < 1].index, inplace=True)
     songs_b.sort_values(by=["counts"], inplace=True, ascending=False)
@@ -72,10 +77,10 @@ def inf(autohr): #Слздаём DataFrame файл, который содежр
 
 def count_words(data, wordik):
     list_of_w = []
-    w = wordik.lower()#Уменьшаем регистр каждого слова для облегчения подсчета
+    w = wordik.lower()  # Уменьшаем регистр каждого слова для облегчения подсчета
     for index, row in data.iterrows():
         cnt = 0
-        lyrics = row['lyrics'].lower().split()#Разделяем каждое слово для его подсчёта
+        lyrics = row['lyrics'].lower().split()  # Разделяем каждое слово для его подсчёта
         for word in lyrics:
             if word == w:
                 cnt += 1
@@ -83,29 +88,31 @@ def count_words(data, wordik):
     return list_of_w
 
 
-def visual(data, list_of_word, wordik):
+def visual(data, list_of_word, wordik):  # Визуализация подсчёта слов
     data[f"Количество {wordik}"] = list_of_word
-    data.sort_values(by=[f"Количество {wordik}"], ascending=False)
-    chart = alt.Chart(data[:15]).mark_bar().encode(
-        alt.X("title", sort=alt.EncodingSortField(field=f'Количество {wordik}', op='sum', order='descending')),
-        alt.Y(f'Количество {wordik}')
-    )
-    altair_viewer.show(chart)
+    d_sort = data.sort_values(by=[f"Количество {wordik}"], ascending=False)
+    data = d_sort.head(5)
+    plt.figure(figsize=(12, 6))
+    plt.bar(data['title'], data[f"Количество {wordik}"])
+    plt.xlabel('Слово')
+    plt.ylabel('Количество')
+    plt.title('Количество слов')
+    plt.show()
 
 
-def max_words_album(album, data):
+def max_words_album(album, data):  # Функция для подсчёта слов в альбоме
     album = data.loc[data.album == album]
-    vectorizer = CountVectorizer(stop_words=stop)
+    vectorizer = CountVectorizer(stop_words=stop)  # Присуждаем каждому слову номер для облегчения подсчёта
     data_set = vectorizer.fit_transform(album.lyrics)
     sum_cnt = data_set.sum(axis=0)
     vocab = vectorizer.vocabulary_
     list_of_tuple = vocab.items()
     master_list = [(word, sum_cnt[0, index])for word, index in list_of_tuple]
     master_list.sort(key=lambda x: x[1], reverse=True)
-    return master_list,len(master_list)
+    return master_list, len(master_list)
 
 
-def max_words_song(data):
+def max_words_song(data):  # Функция для подсчёта всех слов
     vectorizer = CountVectorizer(stop_words=stop)
     data_set = vectorizer.fit_transform(data.lyrics)
     sum_cnt = data_set.sum(axis=0)
@@ -116,73 +123,30 @@ def max_words_song(data):
     print(len(data.lyrics))
     return master_list
 
-def coeff(df):
-    tfid_vect = TfidfVectorizer(stop_words=stop, max_df=.2, min_df=4)
+
+def coeff(df):  # Высчитываем коэффиценты слов, то есть то насколько часто автор использует это слово во всех песнях
+    tfid_vect = TfidfVectorizer(stop_words=stop, max_df=.4, min_df=5)  # Создаём матрицу,оценивая важность слов
     data_set = tfid_vect.fit_transform(df.lyrics)
     sum_cnt1 = data_set.sum(axis=0)
     list_of_tuple1 = tfid_vect.vocabulary_.items()
     master_list1 = [(word, sum_cnt1[0, index])for word, index in list_of_tuple1]
     master_list1.sort(key=lambda x: x[1], reverse=True)
-    return master_list1[:20]
-def wc(data):
-    data=data
-    list_of_word=max_words_song(data)
-    print(list_of_word)
+    format_master_list1 = [(word, f"{value:.2f}") for word, value in master_list1]
+    return format_master_list1[:20]
+
+
+def wc(data):  # Оцениваем эмоциональную окраску текстов
+    list_of_word = max_words_song(data)
     analyzer = SentimentIntensityAnalyzer()
     for word in range(len(list_of_word)):
         vs = analyzer.polarity_scores(translate_text(str(list_of_word[word][0])))
         print("{:-<30} {}".format(list_of_word[word][0], str(vs)))
 
-    return 'hoorray'
-def main():
-    country = input("Автор западный(з) или отечественный(о)?")
-    autohr = input("Введите автора для анализа:")
-    if os.path.exists(f"Lyrics_{autohr}.json"):
-        action = input("Что вы хотите сделать:\nПодсчитать количество использования определенного слова в альбоме автора(1)\nПодсчитать количество слов в альбоме(2)\nПодсчитать коэффицент важности слов для автора(3)\nПодсчитать общее количество слов в текстах автора(4)\n")
 
-        if action == "1" and country == "о":
-            data = inf(autohr)
-
-            wordik = input("Какое слово вы хотите подсчитать:")
-            word = morph.parse(wordik)[0]
-            mass_word = count_words(data, wordik)
-            morphling = ["nomn", 'gent', 'ablt', 'accs', 'loct']
-            povtor = []
-            for i in range(len(morphling)):
-                for j in range(len(morphling[i+1:])):
-                    if word.inflect({morphling[i]}).word == word.inflect({morphling[j]}).word:
-                        povtor.append(morphling[i])
-            res = [x for x in morphling if not any([x.find(y) >= 0 for y in set(povtor)])]
-            for i in res:
-                q = count_words(data, word.inflect({i}).word)
-                for j in range(len(mass_word)):
-                    mass_word[j] = mass_word[j]+q[j]
-            visual(data, mass_word, wordik)
-        elif action == "1" and country == "з":
-            data = inf(autohr)
-            word = input("Какое слово вы хотите подсчитать? ")
-            visual(data, count_words(data, word), word)
-        elif action == "2":
-
-            data = inf(autohr)
-            album = input('В каком альбоме? ')
-            print(max_words_album(album, data))
-
-        elif action == "3":
-
-            data = inf(autohr)
-            print(coeff(data))
-        elif action == "4":
-            data = inf(autohr)
-            print(max_words_song(data))
-        elif action == "5":
-            data = inf(autohr)
-            print(wc(data))
-    else:
-        print("Не знали о таком исполнителе, сейчас узнаем...")
-        down(autohr)
-        main()
-
-
-if __name__ == "__main__":
-    main()
+def cloud(data):  # Создаём кластеры слов автора
+    data = ' '.join(data['lyrics'])
+    wordclouds = WordCloud(width=800, height=800, background_color='white', stopwords=stop).generate(data)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordclouds, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
