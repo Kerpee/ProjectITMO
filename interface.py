00c7c026b4c7d94d738ff5910443778838f6f810
Max <7enidomaks@gmail.com>
2024-06-12 22:59:43 +0300

    Добавление кнопок для новй функций ver=0.25

--- interface.py
+++ interface.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-import os
+
 from analyz import *
+
 
 # Начальный класс, получающий начальную информацию об операции
 
@@ -11,15 +12,16 @@ class Analyzer:
         self.author = author
         self.data = None
         self.down_data()
-        
+
     def down_data(self):  # Функция, скачивающая текста автора, если таких нет, иначе собирает информацию
-        author_files = [f for f in os.listdir() if f.startswith('Lyrics_') and f.endswith('.json')]
+        author_files = [f for f in os.listdir('lyrics') if f.startswith('Lyrics_') and f.endswith('.json')]
         authors_list = [f[7:-5] for f in author_files]
         self.author = get_author(self.author, authors_list)
-        if not os.path.exists(f'Lyrics_{self.author}.json'):
-            messagebox.showinfo('ОЙ','Не знали о таком исполнителе; Сейчас узнаем')
+        if not os.path.exists(os.path.join('lyrics', f'Lyrics_{self.author}.json')):
+            messagebox.showinfo('ОЙ', 'Не знали о таком исполнителе; Сейчас узнаем')
             down(self.author)
         self.data = inf(self.author)
+
 
 # Класс, анализирующий тексты песней
 
@@ -44,7 +46,7 @@ class LyricAnalyzer(Analyzer):
         else:
             visual(self.data, count_words(self.data, word), word)
 
-#  Вызов функций из analyz.py для необоходимых операций
+    #  Вызов функций из analyz.py для необоходимых операций
     def count_words_in_album(self, album):
         return max_words_album(album, self.data)
 
@@ -59,6 +61,14 @@ class LyricAnalyzer(Analyzer):
 
     def visualize_words(self):
         cloud(self.data)
+
+
+def update_all():
+    author_files = [f for f in os.listdir('lyrics') if f.startswith('Lyrics_') and f.endswith('.json')]
+    authors_list = [f[7:-5] for f in author_files]
+    for author in authors_list:
+        down(author)
+    messagebox.showinfo('Пабеда', 'Всё обновилось')
 
 
 class LyricsAnalyzerApp(tk.Tk):  # Класс, инициализирующая интерфейс программы
@@ -77,10 +87,10 @@ class LyricsAnalyzerApp(tk.Tk):  # Класс, инициализирующая 
         self.geometry('700x400')
         self.resizable(False, False)
         self.create_buttons()
-        
+
     def create_buttons(self):  # Функция для создания интерактивных элементов
         frame = ttk.Frame(self, padding="10")
-        frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        frame.grid(row=0, column=0, sticky=(tk.W+tk.E+tk.N+tk.S))
 
         self.country_var = tk.StringVar()
         self.author_var = tk.StringVar()
@@ -95,7 +105,7 @@ class LyricsAnalyzerApp(tk.Tk):  # Класс, инициализирующая 
 
         ttk.Label(frame, text="Введите автора для анализа:").grid(column=1, row=2, sticky=tk.W)
         self.author_entry = ttk.Entry(frame, width=20, textvariable=self.author_var)
-        self.author_entry.grid(column=2, row=2, columnspan=2, sticky=(tk.W, tk.E))
+        self.author_entry.grid(column=2, row=2, columnspan=2, sticky=(tk.W+tk.E))
 
         ttk.Label(frame, text="Что вы хотите сделать:").grid(column=1, row=3, sticky=tk.W)
         self.action_menu = ttk.Combobox(frame, textvariable=self.action_var)
@@ -105,17 +115,21 @@ class LyricsAnalyzerApp(tk.Tk):  # Класс, инициализирующая 
                                       "Подсчёт всех слов исполнителя",
                                       "Высчёт эмоциональной окраски слова",
                                       'Показать визуализацию слов')
-        self.action_menu.grid(column=2, row=3, columnspan=2, sticky=(tk.W, tk.E))
+        self.action_menu.grid(column=2, row=3, columnspan=2, sticky=(tk.W+tk.E))
 
-        ttk.Label(frame, text="Введите слово для подсчета:").grid(column=1, row=4,sticky=tk.W)
+        ttk.Label(frame, text="Введите слово для подсчета:").grid(column=1, row=4, sticky=tk.W)
         self.word_entry = ttk.Entry(frame, width=20, textvariable=self.word_var)
-        self.word_entry.grid(column=2, row=4, columnspan=2, sticky=(tk.W, tk.E))
+        self.word_entry.grid(column=2, row=4, columnspan=2, sticky=(tk.W+tk.E))
 
         ttk.Label(frame, text="Введите название альбома:").grid(column=1, row=5, sticky=tk.W)
         self.album_entry = ttk.Entry(frame, width=20, textvariable=self.album_var)
-        self.album_entry.grid(column=2, row=5, columnspan=2, sticky=(tk.W, tk.E))
+        self.album_entry.grid(column=2, row=5, columnspan=2, sticky=(tk.W+tk.E))
 
         ttk.Button(frame, text="Начать анализ", command=self.start_analyzis).grid(column=2, row=6, columnspan=2)
+        ttk.Button(frame, text='Обновить базу для всех исполнителей', command=update_all).grid(column=2, row=7,
+                                                                                               columnspan=3)
+        ttk.Button(frame, text='Обновить базу для одного исполнителя', command=self.update_one).grid(column=2, row=8,
+                                                                                                     columnspan=3)
 
     def start_analyzis(self):  # Функция, выводящая результаты на экран пользователя
         country = self.country_var.get()
@@ -157,7 +171,19 @@ class LyricsAnalyzerApp(tk.Tk):  # Класс, инициализирующая 
         elif action == "Показать визуализацию слов":
             analyzer.visualize_words()
 
+    def update_one(self):
+        author = self.author_var.get()
+        if author:
+            down(author)
+            messagebox.showinfo('Пабеда', 'Всё обновилось')
+        else:
+            messagebox.showwarning('Паражение', 'Введите автора')
 
-if __name__ == "__main__":
+
+def main():
     app = LyricsAnalyzerApp()
     app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
